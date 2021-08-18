5017d21791857c6dc5fa6ca692a1beb969b3e508
AMAYadav <89167553+AMAYadav@users.noreply.github.com>
2021-08-19 05:09:02 +0530

    Update EDA.R

--- EDA.R
+++ EDA.R
@@ -3,12 +3,12 @@ bank_data = bank.full_TTIwFX
 install.packages("DataExplorer")
 install.packages("dlookr")
 install.packages("SmartEDA")
-#DATA EXPLORER PACKAGE
+#DATAEXPLORER  PACKAGE
 library(DataExplorer)
 create_report(bank_data)  #Without Response Varable or Variable of Intrest
 create_report(bank_data,bank_data$education)
 
-#SMART EDA
+#SMARTEDA PACKAGE 
 
 install.packages("SmartEDA")
 library(SmartEDA)
@@ -16,3 +16,17 @@ ExpData(data = bank_data,type = 1)
 ExpReport(bank_data)
 
 ExpReport(bank_data, op_file = "bank_data.html")
+
+#DLOOKR PACKAGE
+install.packages("TREXr") 
+library(dlookr)
+diagnose_report(bank_data)
+
+diagnose_paged_report(bank_data)  #Pdf file
+diagnose_web_report(bank_data)  #PERFECT
+#transformation_web_report(bank_data)
+#transformation_paged_report(bank_data)
+diagnose(bank_data)%>%flextable::flextable()  #useful function
+
+flextable::flextable(bank_data)  #DONT RUN IT MAKES SYSTEM BULKIER ON NORMAL TIME
+
